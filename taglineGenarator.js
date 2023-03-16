b116c2411b43e768bf9072180c2fe2035fbe77fc
Rifat Jayed <rifat.jayed2000@gmail.com>
2023-03-16 10:59:00 +0600

    modal bug and images show bug fixed

--- taglineGenarator.js
+++ taglineGenarator.js
@@ -24,20 +24,17 @@ generateTag(tagLines, 2000);
 document
   .getElementById("random-room-btn")
   .addEventListener("click", async function () {
-    console.log("hello world");
+    // console.log("hello world");
     const data = await fetch(`ROOMS.json`);
     const result = await data.json();
     const randomIndex = Math.floor(Math.random() * 10);
     const modalBody = document.getElementById("random-room-info-modal-body");
-    const { name, summary, property_type, images, review_scores } = result[9];
-    
-    
-
+    const { name, summary, property_type, images, review_scores } = result[randomIndex];
     const reviews = document.createElement("ol");
     if (review_scores.scores) {
       reviews.setAttribute('class',"list-group")
 
-      object.keys(review_scores.scores).forEach((key) => {
+      Object.keys(review_scores.scores).forEach((key) => {
         reviews.innerHTML += `
         <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
         ${key} : 
@@ -48,7 +45,7 @@ document
     } else {
       reviews.innerHTML = "No reviews found";
     }
-    
+
     modalBody.innerHTML = `
     <div class="col">
     <div class="card h-100">
@@ -60,7 +57,7 @@ document
         <p class="card-text">${summary}</p>
         <div id='review-score'>
       Review Scores :
-      ${review_scores.scores.review_scores_accuracy}
+     <div id="reviews"> </div>
     </div>
       </div>
       <button class="btn btn-info btn-lg"  role="button"
@@ -68,7 +65,7 @@ document
     </div>
    </div>
     `;
-    
+    document.getElementById("reviews").appendChild(reviews)
   });
 
 
